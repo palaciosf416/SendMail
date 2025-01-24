43af60a7cf5d514681a8be2b7be9ab9ad0a7cf60
fernando <palaciosf416@gmail.com>
2025-01-24 13:54:35 -0600

    fer

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
